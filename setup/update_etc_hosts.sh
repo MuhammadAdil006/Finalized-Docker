94155977e3b445051c8f944cf7fa2b87628e5f10
MuhammadAdil006 <qadil392@gmail.com>
2023-03-30 08:33:13 -0700

    docker done and chaincodee

---
+++ setup/update_etc_hosts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Update /etc/hosts
+source    ./manage_hosts.sh
+
+HOSTNAME=peer1.excise.com
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
+HOSTNAME=peer1.fbr.com
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
+HOSTNAME=peer1.manufacturer.com
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
+HOSTNAME=orderer.excise.com
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
+HOSTNAME=postgresql
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
+HOSTNAME=explorer
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
+HOSTNAME=vagrant
+removehost $HOSTNAME            &> /dev/null
+addhost $HOSTNAME
